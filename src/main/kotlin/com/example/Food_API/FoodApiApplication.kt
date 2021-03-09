2eaff2f876dcae44aa9e36c2e9be4ad59238fb89
Dalton Natale <cdalton.natale@gmail.com>
2021-03-09 11:38:27 -0700

    creating api wrapper

---
+++ src/main/kotlin/com/example/Food_API/FoodApiApplication.kt
@@ -0,0 +1,11 @@
+package com.example.Food_API
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FoodApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<FoodApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/Food_API/FoodApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.Food_API
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FoodApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
